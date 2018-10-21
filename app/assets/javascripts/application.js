a61debc291f517b3a0816925098240ffff87214d
Jacob Galati <jacob@realfoto.ca>
2018-10-21 07:01:12 +0000

    add bootstrap to application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require popper
+//= require jquery
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
