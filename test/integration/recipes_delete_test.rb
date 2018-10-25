708ccb22eba7cf7f530e2fa1556ce1ee8698dfc8
Jacob Galati <jacob@realfoto.ca>
2018-10-25 15:07:23 +0000

    Implement edit and delete recipes

---
+++ test/integration/recipes_delete_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class RecipesDeleteTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef = Chef.create!(chefname: "Jacob", email: "jacob@test.com")
+    @recipe = Recipe.create(name: "Veggies and Beef", description: "lots of beef and a bit of veggies", chef: @chef)
+  end
+  
+  test "successfully delete a recipe" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+    assert_difference 'Recipe.count', -1 do
+      delete recipe_path(@recipe)
+    end
+    assert_redirected_to recipes_path
+    assert_not flash.empty?
+  end
+end
