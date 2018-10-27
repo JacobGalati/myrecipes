c5ddf6b55e96b6022f5ea08f6f92557a0dd580f1
Jacob Galati <jacob@realfoto.ca>
2018-10-27 06:09:26 +0000

    Complete authenticaion system with tests

--- test/integration/chefs_test.rb
+++ test/integration/chefs_test.rb
@@ -15,4 +15,15 @@ class ChefsTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", chef_path(@chef), @chef.chefname
     assert_select "a[href=?]", chef_path(@chef2), @chef2.chefname
   end
+  
+  test "should delete chef" do
+    sign_in_as(@chef2, "passwords")
+    get chefs_path
+    assert_template 'chefs/index'
+    assert_difference 'Chef.count', -1 do
+      delete chef_path(@chef2)
+    end
+    assert_redirected_to chefs_path
+    assert_not flash.empty?
+  end
 end
