266fa99dfe023f3de4160eef5c4d225e5181a1b3
Jacob Galati <jacob@realfoto.ca>
2018-11-02 17:24:16 +0000

    complete comments feature

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,7 +2,7 @@ class Ingredient < ApplicationRecord
 
 validates :name, presence: true, length: { minimum: 3, maximum: 25 }
 validates_uniqueness_of :name
-
+default_scope -> { order(name: :asc) }
 has_many :recipe_ingredients
 has_many :recipes, through: :recipe_ingredients
 end
